cca9f23f2b910b0db61a4c07b0f70c6a4a115ed2
Andrew <andrewhuang@richitech.com.tw>
2023-01-07 02:16:30 +0800

    api示範

--- pages/[id].vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <h1>動態網址</h1>
-        {{ route.path }}
-    </div>
-</template>
-<script setup>
-    const route = useRoute();
-</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
+<script setup>
+const {data} = await useFetch("https://60bd9841ace4d50017aab3ec.mockapi.io/api/post_card")
+
+onMounted(() => {
+})
+</script>
 <template>
     <h1>首頁</h1>
+	<ul>
+		<li v-for="item in data" :key="item.id">
+			<img :src="item.avatar" alt="">
+			<p>createdAt:{{ item.createdAt }}</p>
+			<p>name:{{ item.name }}</p>
+			<p>post_date: {{ item.post_date }}</p>
+			<p>content: {{ item.content }}</p>
+		</li>
+	</ul>
 </template>
